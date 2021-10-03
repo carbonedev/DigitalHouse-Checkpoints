7d1575925fafd6dc2892142cfd6f7b3b6e456ded
unknown <jr.paulo87@gmail.com>
2021-10-03 12:00:00 -0300

    começando o projeto

---
+++ Front-end/Checkpoint-3/js/app.js
@@ -0,0 +1,12 @@
+//sombra do menu
+function scrollHeader(){
+    const nav = document.getElementById('header');
+
+    if(this.scrollY >= 50) nav.classList.add('active-header');
+    else nav.classList.remove('active-header');
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+
+//dropdown menu
