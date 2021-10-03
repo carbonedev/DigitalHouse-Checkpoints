52bd96bdc208216dbf536f0b205de238b1ba476c
unknown <jr.paulo87@gmail.com>
2021-10-03 17:47:03 -0300

    front-end: checkpoint 3

--- Front-end/Checkpoint-3/js/app.js
+++ Front-end/Checkpoint-3/js/app.js
@@ -1,12 +1,4 @@
-//sombra do menu
-function scrollHeader(){
-    const nav = document.getElementById('header');
-
-    if(this.scrollY >= 50) nav.classList.add('active-header');
-    else nav.classList.remove('active-header');
-}
-
-window.addEventListener('scroll', scrollHeader)
-
-
-//dropdown menu
+var s = skrollr.init({
+    smoothScrolling: false,
+    forceHeight: true
+  });
